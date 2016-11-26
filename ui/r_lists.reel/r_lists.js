4b6dd79b73e6d69ac6c19ca4f98c99da5b4a60db
crouso <noreply>
2016-11-26 22:53:42 +0000

    Add component r_lists

---
+++ ui/r_lists.reel/r_lists.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/r_lists.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class R_lists
+ * @extends Component
+ */
+exports.R_lists = Component.specialize(/** @lends R_lists# */ {
+    constructor: {
+        value: function R_lists() {
+            this.super();
+        }
+    }
+});
